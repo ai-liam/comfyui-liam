9b0ae7d4f065fd49f8b1c4b17b6a5fd606004786
liam <37907909@qq.com>
2024-05-01 14:55:20 +0800

    init: init

--- nodes.py
+++ nodes.py
@@ -1,74 +1,63 @@
 import sys
 import os
-from PIL import Image, ImageOps
-import folder_paths
-import torch
-import numpy as np
-import requests
-from io import BytesIO
+
+_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(_path)
+
+import lib_utils
+
 
 #sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
 
-
-
-def open_image_from_url(url):
-    response = requests.get(url)
-    image_data = BytesIO(response.content)
-    image = Image.open(image_data)
-    return image
-
-def open_image_from_input(file_name):
-    image_path = folder_paths.get_annotated_filepath(file_name)
-    image = Image.open(image_path)
-    return image
-
-def back_image(i):
-    i = ImageOps.exif_transpose(i)
-    image = i.convert("RGB")
-    image = np.array(image).astype(np.float32) / 255.0
-    image = torch.from_numpy(image)[None,]
-    if 'A' in i.getbands():
-        mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-        mask = 1. - torch.from_numpy(mask)
-    else:
-        mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-    return (image, mask.unsqueeze(0))
-
-class LiamLoadImage:
+class ImageToGray:
     @classmethod
     def INPUT_TYPES(s):
-        input_dir = folder_paths.get_input_directory()
-        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required": { "image": ("IMAGE",)}}
+    RETURN_TYPES = ("IMAGE", )
+    FUNCTION = "execute"
+    CATEGORY = "Liam/Image"
+
+    def execute(self,image):
+        print(f"""Your input contains:
+                image: {type(image)}
+            """)
+        img = lib_utils.convert_to_gray_and_return_tensor_pil(image)
+        return (img,)
+        
+
+class LoadImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        # input_dir = folder_paths.get_input_directory()
+        # files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
         return {"required":
                     {"url": ("STRING", {"default": ""}),
-                    "image": (sorted(files), {"image_upload": True})},
+                    # "image": (sorted(files), {"image_upload": True})
+                    },
                 }
-
     RETURN_TYPES = ("IMAGE", "MASK")
-    FUNCTION = "load_image"
-    CATEGORY = "image"
+    FUNCTION = "execute"
+    CATEGORY = "Liam/Image"
 
-
-
-
-    def load_image(self,url, image):
+    def execute(self,url):
         print(f"""Your input contains:
                 url: {url}
-                image: {image}
             """)
-        if url != "" and url.startswith('http'):
-            i = open_image_from_url(url)
-            return back_image(i)
+        if  url.startswith('http'):
+            i = lib_utils.open_image_from_url(url)
+            return lib_utils.back_image(i)
         else:
-            i = open_image_from_input(image)
-            return back_image(i)
+            i = lib_utils.open_image_from_input(url)
+            return lib_utils.back_image(i)
 
 
 
 NODE_CLASS_MAPPINGS = {
-    "LiamLoadImage": LiamLoadImage,
+    "LiamLibLoadImage": LoadImage,
+    "LiamLibImageToGray": ImageToGray,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LiamLoadImage": "LiamLoadImage",
+    "LiamLibLoadImage": "LiamLibLoadImage",
+    "LiamLibImageToGray": "LiamLibImageToGray",
 }
